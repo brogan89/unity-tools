105459824ad1f2c9baadb1faa15d764efe26b437
brogan89 <brogan.king@protonmail.com>
2020-05-03 11:33:49 +1200

    changed custom package stuff to OdinEditorWindow to speed up development

--- Editor/CustomPackageManagerWindow.cs
+++ Editor/CustomPackageManagerWindow.cs
@@ -4,6 +4,8 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using Newtonsoft.Json.Linq;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
 using UnityEditor;
 using UnityEngine;
 using UnityTools;
@@ -12,10 +14,8 @@ using Debug = UnityEngine.Debug;
 
 namespace UnityToolsEditor
 {
-	public class CustomPackageManagerWindow : EditorWindow
+	public class CustomPackageManagerWindow : OdinEditorWindow
 	{
-		private List<PackageInfo> packages;
-		
 		[MenuItem("Window/Custom Package Manager", priority = 103)]
 		private static void Init()
 		{
@@ -33,91 +33,40 @@ namespace UnityToolsEditor
 				.Select(x => new PackageInfo(((JProperty)x).Name, ((JProperty)x).Value.ToString()))
 				.ToList();
 		}
+		
+		[Button, PropertyOrder(0)]
+		private void CreateNewCustomPackage()
+		{
+			NewCustomPackageWindow.ShowWindow(info =>
+			{
+				Debug.Log($"Created new package: {info}");
+			});
+		}
 
+		[Button, PropertyOrder(1)]
 		private void RefreshPackages()
 		{
 			packages = CreatePackageInfo();
 			Debug.Log($"Loaded packages: {packages.ToArrayString()}");
 		}
-
-		private void OnGUI()
-		{
-			if (packages == null)
-			{
-				RefreshPackages();
-				return;
-			}
-
-			GUILayout.Label("Custom Packages", EditorStyles.boldLabel);
-
-			// create new
-			if (GUILayout.Button("Create New"))
-				CreateNewCustomPackage();
-			
-			// refresh
-			if (GUILayout.Button("Refresh Packages"))
-				RefreshPackages();
-
-			// list all custom packages
-			foreach (var pkg in packages)
-			{
-				// package name
-				GUILayout.Label(pkg.isEditing ? $"{pkg.name} (EDITING)" : pkg.name, EditorStyles.boldLabel);
-				GUILayout.Label(pkg.version);
-				
-				// -----------------------------------------------
-
-				// git URL
-				EditorGUILayout.BeginHorizontal();
-				{
-					pkg.url = EditorGUILayout.TextField("URL", pkg.url);
-					if (GUILayout.Button("Open"))
-						pkg.OpenGitUrl();
-				}
-				EditorGUILayout.EndHorizontal();
-				
-				// -----------------------------------------------
-				
-				// local path
-				EditorGUILayout.BeginHorizontal();
-				{
-					pkg.localPath = EditorGUILayout.TextField("Local Path", pkg.localPath);
-			
-					if (GUILayout.Button("Open"))
-						pkg.GoToLocalPath();
-					
-					if (GUILayout.Button("Save Path"))
-						PackageManagerUtils.SaveCustomPackageLocalPath(pkg.name, pkg.localPath);
-					
-					EditorGUILayout.EndHorizontal();
-					
-					var isEditing = EditorGUILayout.Toggle("Edit", pkg.isEditing);
-					if (isEditing != pkg.isEditing)
-					{
-						pkg.isEditing = isEditing;
-						Debug.Log($"Toggle editing: {pkg.isEditing}");
-						PackageManagerUtils.SetPackageValue(pkg.name, pkg.isEditing ? $"file:{pkg.localPath}" : pkg.url);
-						AssetDatabase.Refresh();
-						RefreshPackages();
-					}
-				}
-				
-				// -----------------------------------------------
-			}
-		}
 		
-		private void CreateNewCustomPackage()
-		{
-			Debug.Log("Creating new custom packages is not implemented yet");
-		}
+		[SerializeField, PropertyOrder(2)] 
+		private List<PackageInfo> packages;
 		
 		[Serializable]
 		private class PackageInfo
 		{
 			public string name;
 			public string version;
+			
+			[InlineButton(nameof(GoToLocalPath), "Open")]
+			[InlineButton(nameof(SaveLocalPath), "Save")]
 			public string localPath;
+			
+			[InlineButton(nameof(OpenGitUrl), "Open")]
 			public string url;
+			
+			[DisableIf("@string.IsNullOrEmpty(localPath)")]
 			public bool isEditing;
 
 			public PackageInfo(string name, string path)
@@ -162,6 +111,11 @@ namespace UnityToolsEditor
 			{
 				Debug.Log($"Open folder: {localPath}");
 				Process.Start(localPath.Replace("file:", ""));
+			}
+
+			public void SaveLocalPath()
+			{
+				PackageManagerUtils.SaveCustomPackageLocalPath(name, localPath);
 			}
 
 			public void OpenGitUrl()
--- Runtime/PackageManagerUtils.cs
+++ Runtime/PackageManagerUtils.cs
@@ -15,6 +15,12 @@ namespace UnityTools
 		private static JObject _customPackages;
 		private static JObject CustomPackages => _customPackages ?? (_customPackages = JObject.Parse(File.ReadAllText(CustomPackagePath)));
 
+		/// <summary>
+		/// Returns the local directory editing path specified in manifest.json
+		/// </summary>
+		/// <param name="packageName"></param>
+		/// <param name="path"></param>
+		/// <returns></returns>
 		public static bool TryGetEditingPath(string packageName, out string path)
 		{
 			var packagePath = Manifest["dependencies"][packageName]?.ToString();
@@ -26,30 +32,52 @@ namespace UnityTools
 			return !string.IsNullOrEmpty(path);
 		}
 
+		/// <summary>
+		/// Ret
+		/// </summary>
+		/// <param name="packageName"></param>
+		/// <returns></returns>
 		public static bool IsEditingPackage(string packageName)
 		{
 			return TryGetEditingPath(packageName, out _);
 		}
 
+		/// <summary>
+		/// Returns SHA1 of package.
+		/// NOTE: only works outside of edit mode
+		/// </summary>
+		/// <param name="packageName"></param>
+		/// <returns></returns>
 		public static string GetSHA1(string packageName)
 		{
-			if (IsEditingPackage(packageName))
-				return null;
-			
-			return Manifest?["lock"]?[packageName]?["hash"]?.ToString();
+			return IsEditingPackage(packageName) ? null : Manifest?["lock"]?[packageName]?["hash"]?.ToString();
 		}
 
+		/// <summary>
+		/// Sets the value of the packageName in manifest.json
+		/// This can be either 'file:{localPath}' or '{gitUrl}'
+		/// </summary>
+		/// <param name="packageName"></param>
+		/// <param name="value"></param>
 		public static void SetPackageValue(string packageName, string value)
 		{
 			Manifest["dependencies"][packageName] = value;
 			WriteManifest();
 		}
 
+		/// <summary>
+		/// Saves current Manifest data to manifest.json file in Packages/
+		/// </summary>
 		private static void WriteManifest()
 		{
 			File.WriteAllText(PACKAGE_PATH, Manifest.ToString());
 		}
 
+		/// <summary>
+		/// Saves local file directory to custom-packages.json in persistent data folder
+		/// </summary>
+		/// <param name="packageName"></param>
+		/// <param name="localPath"></param>
 		public static void SaveCustomPackageLocalPath(string packageName, string localPath)
 		{
 			if (!File.Exists(CustomPackagePath))
@@ -60,25 +88,38 @@ namespace UnityTools
 			File.WriteAllText(CustomPackagePath, CustomPackages.ToString());
 		}
 
+		/// <summary>
+		/// Returns the saved local path specified in CustomPackageManagerWindow
+		/// </summary>
+		/// <param name="packageName"></param>
+		/// <returns></returns>
 		public static string GetSavedLocalPath(string packageName)
 		{
-			var path = !File.Exists(CustomPackagePath) && CustomPackages != null ? null : CustomPackages[packageName]?.ToString();
+			var path = !File.Exists(CustomPackagePath) || CustomPackages == null 
+				? null 
+				: CustomPackages[packageName]?.ToString();
 			
 			Debug.Log(!string.IsNullOrEmpty(path)
-				? $"Saving local path. <b>{packageName}: {path}</b>"
-				: $"Local path not saved for package '{packageName}'");
+				? $"Loaded local path. <b>{packageName}: {path}</b>"
+				: $"Local path not found for package '{packageName}'");
 		
 			return path;
 		}
 
+		/// <summary>
+		/// Returns path to cached package in Library folder.
+		/// NOTE: only used outside of editing directory
+		/// </summary>
+		/// <param name="packageName"></param>
+		/// <returns></returns>
 		public static string GetCachedPackagePath(string packageName)
 		{
 			if (!IsEditingPackage(packageName))
 			{
 				var sha = GetSHA1(packageName)?.Substring(0, 10);
-				if (!string.IsNullOrEmpty(sha))
-					return $"Library/PackageCache/{packageName}@{sha}";
-				return null;
+				return !string.IsNullOrEmpty(sha)
+					? $"Library/PackageCache/{packageName}@{sha}"
+					: null;
 			}
 			
 			Debug.LogError($"Package '{packageName}' is in edit mode. Use package local path");
