f7b7552e2071ec7427e447828f1670c4f844504f
brogan89 <brogan.king@outlook.com>
2020-05-11 20:56:10 +1200

    1.2.1 - added generic unity events

---
+++ Runtime/GenericUnityEvents.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace UnityTools
+{
+	[Serializable] public class IntEvent : UnityEvent<int> { }
+	[Serializable] public class FloatEvent : UnityEvent<float> { }
+	[Serializable] public class BoolEvent : UnityEvent<bool> { }
+	[Serializable] public class StringEvent : UnityEvent<string> { }
+	[Serializable] public class GameObjectEvent : UnityEvent<GameObject> { }
+	[Serializable] public class TransformEvent : UnityEvent<Transform> { }
+	[Serializable] public class RectTransformEvent : UnityEvent<RectTransform> { }
+	
+	// ... add more events as you need them
+}
