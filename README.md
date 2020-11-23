2940908456f17331da74ea39cf47c9879e0788a4
brogan89 <brogan.king@outlook.com>
2020-11-24 00:05:13 +1300

    1.2.7 - Removed Wait.cs, added Debounce class

--- README.md
+++ README.md
@@ -7,5 +7,4 @@ Collection of scripts that are useful for Unity projects
 - Debugging Enhancements - Adds class awareness to console logs making it easier to filter messages in the Unity Console
 - Publisher/Subscriber messaging system - A simple messaging system useful for broadcasting data to UI minimizing coupling.
 - Singleton - Wraps any MonoBehaviour into a singleton
-- Wait - Quick way of calling `yield return new WaitForSeconds()` or `yield return new WaitUntil()` without creating a coroutine
 - SelectableGroup - change other Graphic targets based on a single `UnityEngine.Selectable` event callbacks
---
+++ Runtime/Debounce.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace UnityTools
+{
+	public class Debounce
+	{
+		private readonly float _debounceTime;
+		private readonly Action _callback;
+
+		private float _currentTime;
+		private bool _isTriggered;
+
+		public Debounce(float debounceTime, Action callback)
+		{
+			_debounceTime = debounceTime;
+			_callback = callback;
+		}
+		
+		public void Tick(float dt)
+		{
+			if (_isTriggered)
+				return;
+			
+			_currentTime += dt;
+
+			if (_currentTime > _debounceTime)
+			{
+				_callback?.Invoke();
+				_isTriggered = true;
+			}
+		}
+
+		public void Reset()
+		{
+			_isTriggered = false;
+			_currentTime = 0;
+		}
+	}
+}
