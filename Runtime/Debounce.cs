3e4adb0a328635bf127e70a408eb76815560dda0
brogan89 <brogan.king@outlook.com>
2020-12-23 14:04:28 +1300

    1.2.9 - Improved Debounce

--- Runtime/Debounce.cs
+++ Runtime/Debounce.cs
@@ -1,36 +1,56 @@
 ﻿using System;
+using System.Collections;
+using UnityEngine;
 
 namespace UnityTools
 {
+	/// <summary>
+	/// A simple debounce implementation which helps with limiting spamming a method or action.
+	/// </summary>
 	public class Debounce
 	{
+		private readonly MonoBehaviour _monoBehaviour;
 		private readonly float _debounceTime;
 		private readonly Action _callback;
 
 		private float _currentTime;
 		private bool _isTriggered;
 
-		public Debounce(float debounceTime, Action callback)
+		public Debounce(MonoBehaviour monoBehaviour, float debounceTime, Action callback)
 		{
+			_monoBehaviour = monoBehaviour;
 			_debounceTime = debounceTime;
 			_callback = callback;
+			_isTriggered = true;
+			_monoBehaviour.StartCoroutine(Tick());
 		}
 		
-		public void Tick(float dt)
+		private IEnumerator Tick()
 		{
-			if (_isTriggered)
-				return;
-			
-			_currentTime += dt;
-
-			if (_currentTime > _debounceTime)
+			while (_monoBehaviour)
 			{
-				_callback?.Invoke();
-				_isTriggered = true;
+				if (_isTriggered)
+				{
+					yield return null;
+					continue;
+				}
+				
+				_currentTime += Time.unscaledDeltaTime;
+
+				if (_currentTime > _debounceTime)
+				{
+					_callback?.Invoke();
+					_isTriggered = true;
+				}
+				
+				yield return null;
 			}
 		}
 
-		public void Reset()
+		/// <summary>
+		/// Ping the debounce. The will start time timer.
+		/// </summary>
+		public void Ping()
 		{
 			_isTriggered = false;
 			_currentTime = 0;
