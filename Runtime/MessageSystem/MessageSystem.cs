1260064d01c959353ccf3c308b83c89b3cecf70f
brogan89 <brogan.king@outlook.com>
2021-02-09 23:19:55 +1300

    1.2.11 - Added option to event system for only invoking when caller is enabled

--- Runtime/MessageSystem/MessageSystem.cs
+++ Runtime/MessageSystem/MessageSystem.cs
@@ -15,27 +15,56 @@ namespace UnityTools.MessageSystem
 	/// </summary>
 	public static class EventMessage
 	{
+		/// <summary>
+		/// Subscriber data
+		/// </summary>
 		private struct Subscriber
 		{
+			/// <summary>
+			/// The script which the event has been subscribed
+			/// </summary>
 			public MonoBehaviour caller;
+			
+			/// <summary>
+			/// The callback for the published event
+			/// </summary>
 			public Delegate callback;
+			
+			/// <summary>
+			/// If true will only invoke callback if caller if enabled
+			/// </summary>
+			public bool enabledOnly;
 		}
 
 		private static readonly Dictionary<Type, List<Subscriber>> Subscribers = new Dictionary<Type, List<Subscriber>>();
 
 		#region Subscribe
 
-		public static void Subscribe<T>(MonoBehaviour caller, Action callback) where T : IMessage
+		/// <summary>
+		/// Subscribe a callback to an event
+		/// </summary>
+		/// <param name="caller"></param>
+		/// <param name="callback"></param>
+		/// <param name="enabledOnly">Only invoke callback if MonoBehaviour is enabled</param>
+		/// <typeparam name="T"></typeparam>
+		public static void Subscribe<T>(MonoBehaviour caller, Action callback, bool enabledOnly = false) where T : IMessage
 		{
-			AddSubscriber<T>(caller, callback);
+			AddSubscriber<T>(caller, callback, enabledOnly);
 		}
 
-		public static void Subscribe<T>(MonoBehaviour caller, Action<T> callback) where T : IMessage
+		/// <summary>
+		/// Subscribe a callback to an event
+		/// </summary>
+		/// <param name="caller"></param>
+		/// <param name="callback"></param>
+		/// <param name="enabledOnly">Only invoke callback if MonoBehaviour is enabled</param>
+		/// <typeparam name="T"></typeparam>
+		public static void Subscribe<T>(MonoBehaviour caller, Action<T> callback, bool enabledOnly = false) where T : IMessage
 		{
-			AddSubscriber<T>(caller, callback);
+			AddSubscriber<T>(caller, callback, enabledOnly);
 		}
 
-		private static void AddSubscriber<T>(MonoBehaviour caller, Delegate callback) where T : IMessage
+		private static void AddSubscriber<T>(MonoBehaviour caller, Delegate callback, bool enabledOnly) where T : IMessage
 		{
 			var type = typeof(T);
 			if (!Subscribers.ContainsKey(type))
@@ -53,11 +82,17 @@ namespace UnityTools.MessageSystem
 			}
 
 			// add new subscriber
-			Subscribers[type].Add(new Subscriber{ caller = caller, callback = callback});
+			var newSub = new Subscriber
+			{
+				caller = caller,
+				callback = callback,
+				enabledOnly = enabledOnly
+			};
+			
+			Subscribers[type].Add(newSub);
 		}
 
 		#endregion
-
 
 		#region Unsubscribe
 
@@ -83,6 +118,7 @@ namespace UnityTools.MessageSystem
 		}
 
 		#endregion
+		
 		public static void Publish<T>() where T : IMessage
 		{
 			Publish(Activator.CreateInstance<T>());
@@ -97,26 +133,27 @@ namespace UnityTools.MessageSystem
 			}
 			
 			var type = typeof(T);
+			
 			if (!Subscribers.ContainsKey(type))
 				return;
 
 			// filter subscribers - removers all null callers
 			Subscribers[type].RemoveAll(x => !x.caller);
+			
 			var subs = Subscribers[type];
 
 			// invoke callbacks
 			foreach (var sub in subs)
 			{
+				// skip if caller is disabled
+				if (sub.enabledOnly && !sub.caller.enabled)
+					continue;
+				
 				if(sub.callback.Method.GetParameters().Length > 0)
 					sub.callback.DynamicInvoke(eventMessage);
 				else
 					sub.callback.DynamicInvoke();
 			}
 		}
-	}
-
-	public class ExampleMessage : IMessage
-	{
-		public object SomeData { get; set; }
 	}
 }
