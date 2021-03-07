4c2cbb8bafe4defbd1f4e08e5984a85f7568b37b
brogan89 <brogan.king@outlook.com>
2021-03-07 13:41:46 +1300

    1.2.17 - Added screen to world context for LookAt2D

--- Runtime/Extentions/TransformExtensions.cs
+++ Runtime/Extentions/TransformExtensions.cs
@@ -4,9 +4,22 @@ namespace UnityTools.Extensions
 {
 	public static class TransformExtensions
 	{
-		public static void LookAt2D(this Transform transform, Vector2 targetPosition, float offsetDeg = 0, bool swapXAndY = false)
+		private static Camera _mainCamera;
+		private static Camera _MainCamera => _mainCamera ? _mainCamera : _mainCamera = Camera.main;
+		
+		/// <summary>
+		/// Points a transform to a target with 2D context
+		/// </summary>
+		/// <param name="transform"></param>
+		/// <param name="targetPosition"></param>
+		/// <param name="offsetDeg"></param>
+		/// <param name="swapXAndY"></param>
+		/// <param name="screenToWorld">Converts transform position from screen space to world space. Useful for pointing UI arrows at world targets</param>
+		public static void LookAt2D(this Transform transform, Vector2 targetPosition, float offsetDeg = 0, bool swapXAndY = false, bool screenToWorld = false)
 		{
-			var dir = targetPosition - (Vector2) transform.position;
+			var pos = screenToWorld ? _MainCamera.ScreenToWorldPoint(transform.position) : transform.position;
+			
+			var dir = targetPosition - (Vector2)pos;
 			var rad = swapXAndY ? Mathf.Atan2(dir.x, dir.y) : Mathf.Atan2(dir.y, dir.x);
 			var deg = rad * Mathf.Rad2Deg;
 			transform.rotation = Quaternion.Euler(0, 0, deg + offsetDeg);
