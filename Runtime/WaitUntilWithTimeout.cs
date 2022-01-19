55915d8f8f8e28362f8d246f337a6be1cefd1cd6
brogan89 <brogan.king@outlook.com>
2022-01-20 10:11:20 +1300

    Added a new WaitUntilWithTimeout custom yield instruction

---
+++ Runtime/WaitUntilWithTimeout.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace UnityTools
+{
+	/// <summary>
+	///   <para>Suspends the coroutine execution until the supplied delegate evaluates to true or the given time has expired</para>
+	/// </summary>
+	public sealed class WaitUntilWithTimeout : CustomYieldInstruction
+	{
+		private readonly Func<bool> _predicate;
+		private float _currentTime;
+		private readonly Object _context;
+
+		public override bool keepWaiting
+		{
+			get
+			{
+				if (_predicate())
+					return false;
+
+				_currentTime -= Time.deltaTime;
+
+				if (_currentTime <= 0)
+				{
+					Debug.LogWarning("Wait until timed out", _context);
+					return false;
+				}
+
+				return true;
+			}
+		}
+
+		public WaitUntilWithTimeout(Func<bool> predicate, float timeout, Object context = null)
+		{
+			_predicate = predicate;
+			_currentTime = timeout;
+			_context = context;
+		}
+	}
+}
